ddc532461c7d57cb41f76c1d1a9bf18c6f7953ab
NAVEENS-K <knaveens408@gmail.com>
2025-09-23 19:09:19 +0530

    first commit

---
+++ backend/tests/tasks.test.js
@@ -0,0 +1,29 @@
+const request = require("supertest");
+const {server, app} = require("../index");
+const mongoose = require("mongoose");
+
+describe("GET api/tasks", ()=>{
+it("it should be return 200 ok", async() =>{
+
+    const res = await request(app).get("/api/tasks")
+    expect(res.statusCode).toBe(200);
+ 
+
+})
+
+it("it should be return Array data", async() =>{
+
+    const res = await request(app).get("/api/tasks")
+   
+    expect(typeof res.body).toBe("object");
+    expect(res.body).toHaveProperty("tasks");
+
+})
+
+})
+
+afterAll(async()=>{
+    await mongoose.connection.close();
+  await server.close();
+
+})
